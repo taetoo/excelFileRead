ab3f4b387e28d4df2ddf4e5c0babf5b5f5b422ac
taetoo <rlaougs5@naver.com>
2022-09-07 13:43:43 +0900

    Initial commit

---
+++ src/main/java/com/example/excelfileread/ExcelFileReadApplication.java
@@ -0,0 +1,13 @@
+package com.example.excelfileread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelFileReadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelFileReadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/excelfileread/ExcelFileReadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.excelfileread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelFileReadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
