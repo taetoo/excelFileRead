0ff219acea6aab30ff7332b9eb6bb3812cbc01c9
taehyeonkim <rlaougs5@naver.com>
2022-09-14 16:45:34 +0900

    ready to excelFile parsing

--- src/main/java/com/example/excelfileread/ExcelRead.java
+++ src/main/java/com/example/excelfileread/ExcelRead.java
@@ -1,72 +1,73 @@
 package com.example.excelfileread;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 
-import java.io.File;
-import java.io.FileInputStream;
-
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ExcelRead {
 
-    public static void main(String[] args) {
+    public static List<Map<String, String>> read(ReadOption readOption){
 
-        try {
-            FileInputStream file = new FileInputStream("/Users/taehyeonkim/Desktop/incuvers/통합고지 가스전기데이터/전기계약자 DB.xlsx");
-            XSSFWorkbook workbook = new XSSFWorkbook(file);
+        Workbook wb = FileType.getWorkbook(readOption.getFilePath());
 
-            int rowindex=0;
-            int columnindex=0;
-            //시트 수 (첫번째에만 존재하므로 0을 준다)
-            //만약 각 시트를 읽기위해서는 FOR문을 한번더 돌려준다
-            XSSFSheet sheet=workbook.getSheetAt(0);
-            //행의 수
-            int rows=sheet.getPhysicalNumberOfRows();
-            for(rowindex=0;rowindex<rows;rowindex++){
-                //행을읽는다
-                XSSFRow row=sheet.getRow(rowindex);
-                if(row !=null){
-                    //셀의 수
-                    int cells=row.getPhysicalNumberOfCells();
-                    for(columnindex=0; columnindex<=cells; columnindex++){
-                        //셀값을 읽는다
-                        XSSFCell cell=row.getCell(columnindex);
-                        String value="";
-                        //셀이 빈값일경우를 위한 널체크
-                        if(cell==null){
-                            continue;
-                        }else{
-                            //타입별로 내용 읽기
-                            switch (cell.getCellType()){
-                                case XSSFCell.CELL_TYPE_FORMULA:
-                                    value=cell.getCellFormula();
-                                    break;
-                                case XSSFCell.CELL_TYPE_NUMERIC:
-                                    value=cell.getNumericCellValue()+"";
-                                    break;
-                                case XSSFCell.CELL_TYPE_STRING:
-                                    value=cell.getStringCellValue()+"";
-                                    break;
-                                case XSSFCell.CELL_TYPE_BLANK:
-                                    value=cell.getBooleanCellValue()+"";
-                                    break;
-                                case XSSFCell.CELL_TYPE_ERROR:
-                                    value=cell.getErrorCellValue()+"";
-                                    break;
-                            }
-                        }
-                        System.out.println(rowindex+"번 행 : "+columnindex+"번 열 값은: "+value);
+        Sheet sheet = wb.getSheetAt(0);
+
+        int numOfRows = sheet.getPhysicalNumberOfRows();
+        int numOfCells = 0;
+
+        Row row = null;
+        Cell cell = null;
+
+        String cellName = "";
+
+        Map<String, String> map = null;
+
+        List<Map<String, String>> result = new ArrayList<Map<String, String>>();
+
+        for(int rowIndex = readOption.getStartRow() -1; rowIndex < numOfRows; rowIndex++){
+            row = sheet.getRow(rowIndex);
+
+            if(row != null){
+                numOfCells = row.getPhysicalNumberOfCells();
+
+                map = new HashMap<String, String>();
+
+                for(int cellIndex = 0; cellIndex < numOfCells; cellIndex++ ){
+                    cell = row.getCell(cellIndex);
+
+                    cellName = CellRef.getname(cell, cellIndex);
+
+                    if(!readOption.getOutputColumns().contains(cellName)){
+                        continue;
                     }
 
+                    map.put(cellName, CellRef.getValue(cell));
                 }
+                result.add(map);
             }
-
-        }catch(Exception e) {
-            e.printStackTrace();
         }
-
+        return result;
     }
+
+    public static void main(String[] args) {
+
+        ReadOption ro = new ReadOption();
+        ro.setFilePath("");
+//        ro.setOutputColumns();
+        ro.setStartRow(1);
+
+        List<Map<String,String>> result = ExcelRead.read(ro);
+
+        for(Map<String,String> map : result){
+            System.out.println(map.get("A"));
+        }
+    }
+
 
 }
---
+++ src/main/java/com/example/excelfileread/FileType.java
@@ -0,0 +1,43 @@
+package com.example.excelfileread;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileType {
+
+    public static Workbook getWorkbook(String filePath) {
+
+        FileInputStream fis = null;
+
+        try{
+            fis = new FileInputStream(filePath);
+
+        } catch (FileNotFoundException e){
+            throw new RuntimeException(e.getMessage(), e);
+        }
+
+        Workbook wb = null;
+
+        if(filePath.toUpperCase().endsWith("")){
+            try{
+                wb = new HSSFWorkbook(fis);
+            } catch (IOException e) {
+                throw new RuntimeException(e.getMessage(), e);
+            }
+        }
+        else if (filePath.toUpperCase().endsWith("")){
+            try{
+                wb = new XSSFWorkbook(fis);
+            } catch (IOException e) {
+                throw new RuntimeException(e.getMessage(), e);
+            }
+        }
+        return wb;
+    }
+
+}
